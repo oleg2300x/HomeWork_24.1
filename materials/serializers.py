c221d0bb6fbf70a360313a45b17304132d75ed1a
Oleg <panasenko23o@mail.ru>
2024-02-15 20:00:05 +1000

    fix serializers

--- materials/serializers.py
+++ materials/serializers.py
@@ -3,12 +3,12 @@ from rest_framework import serializers
 from materials.models import Course, Lesson
 
 
-class CourseSerializer(serializers.Serializer):
+class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = '__all__'
 
-class LessonSerializer(serializers.Serializer):
+class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
